162821bd0f6eca92a856969cef5efad346f521e3
Krumon <enriqueruedamontero@gmail.com>
2022-06-29 13:36:39 +0200

    Preparing the development environment.

---
+++ Nt.Core/Test.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NinjaTrader.NinjaScript;
+
+namespace Nt.Core
+{
+    public class Test : NinjaScriptBase
+    {
+        #region Implementation methods
+
+        public override string LogTypeName => throw new NotImplementedException();
+
+        #endregion
+
+        #region Override methods
+
+        protected override void OnBarUpdate()
+        {
+            base.OnBarUpdate();
+        }
+        
+        #endregion
+
+    }
+}
