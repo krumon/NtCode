954638f94e0396befa5860f2662ff2427852aa3c
Krumon <enriqueruedamontero@gmail.com>
2022-07-01 00:28:12 +0200

    Creating session classes.

---
+++ Nt.Core/Sessions/TradingSession.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace NtCore
+{
+    /// <summary>
+    /// Represents the session definition.
+    /// </summary>
+    public class TradingSession
+    {
+
+        #region Public properties
+
+        /// <summary>
+        /// Represents the session time zone.
+        /// </summary>
+        public TradingTimeZone TimeZone { get; set; }
+
+        #endregion
+
+        #region Constructors
+
+        /// <summary>
+        /// Create default instance of <see cref="TradingSession"/> class.
+        /// </summary>
+        public TradingSession()
+        {
+
+        }
+
+        #endregion
+
+    }
+}
