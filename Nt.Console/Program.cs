ac576469a36a09b1dc503fc672544307cae38aac
Krumon <enriqueruedamontero@gmail.com>
2022-07-20 03:05:12 +0200

    Creating Ninjatrader connection.

--- Nt.Console/Program.cs
+++ Nt.Console/Program.cs
@@ -7,7 +7,7 @@ namespace NtConsole
 {
     internal class Program
     {
-        public static Client client = new Client();
+        public static Client client;
         public static Timer timer;
 
         static void Main(string[] args)
@@ -15,10 +15,11 @@ namespace NtConsole
             //foreach(var timeZone in TradingSession.Asian.ToArray())
             //    Console.WriteLine(timeZone.ToSessionHours().ToString());
 
+            client = new Client();
             int connect = client.Connected(1);
-            Console.WriteLine(String.Format("{0} | Connected to NT8: {1}",DateTime.Now,connect.ToString()));
-
             client.SubscribeMarketData("MES");
+
+            Console.WriteLine(String.Format("{0} | Connected to NT8: {1}",DateTime.Now,connect.ToString()));
 
             timer = new Timer()
             {
@@ -28,23 +29,38 @@ namespace NtConsole
             timer.Elapsed += Timer_Elapsed;
             timer.Enabled = true;
 
-            Console.ReadKey();
+            Console.WriteLine("Antes del red key");
 
+            Console.ReadKey();
+            
+            Console.WriteLine("Después del red key");
+            
             client.UnsubscribeMarketData("MES");
-            timer.Enabled = false;
             timer.Elapsed -= Timer_Elapsed;
+            timer.Enabled = false;
             timer.Dispose();
 
+            Console.WriteLine("Fin del programa.");
+            if (timer != null)
+            {
+                Console.WriteLine("El timer sigue activo");
+                Console.WriteLine(timer.Enabled.ToString());
+                Console.WriteLine(timer.Container);
+
+            }
+            timer.Close();
+            int disconnect = client.TearDown();
+            Console.WriteLine(String.Format("{0} | Disconnected to NT8: {1}", DateTime.Now, disconnect.ToString()));
         }
 
         private static void Timer_Elapsed(object sender, ElapsedEventArgs e)
         {
-            if (client == null )
-                return;
+            //if (client == null )
+            //    return;
 
-            double lastPrice = client.MarketData("MES", 0);
+            //double lastPrice = client.MarketData("MES", 0);
 
-            Console.WriteLine(string.Format("{0} | Last: {1}",DateTime.Now,lastPrice));
+            //Console.WriteLine(string.Format("{0} | Last: {1}",DateTime.Now,lastPrice));
         }
     }
 }
