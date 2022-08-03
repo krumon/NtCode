f6e722aa8eb1d6b6925c2bf035f73af662677154
Krumon <enriqueruedamontero@gmail.com>
2022-08-03 17:17:30 +0200

    Creating SessionHour indicator.

--- Nt.Core/Sessions/SessionHours/SessionHours.cs
+++ Nt.Core/Sessions/SessionHours/SessionHours.cs
@@ -145,7 +145,7 @@ namespace NtCore
             TimeZoneInfo sourceTimeZoneInfo = null,
             TimeZoneInfo destinationTimeZoneInfo = null)
         {
-            return BeginSessionTime.GetNextTime(currentDate, sourceTimeZoneInfo, destinationTimeZoneInfo);
+            return BeginSessionTime.GetNextTime(currentDate);
         }
 
         /// <summary>
@@ -160,13 +160,13 @@ namespace NtCore
             TimeZoneInfo destinationTimeZoneInfo = null,
             bool sessionComplete = false)
         {
-            DateTime beginDateTime = BeginSessionTime.GetNextTime(currentDate, sourceTimeZoneInfo, destinationTimeZoneInfo);
-            DateTime endDateTime = EndSessionTime.GetNextTime(currentDate, sourceTimeZoneInfo, destinationTimeZoneInfo);
+            DateTime beginDateTime = BeginSessionTime.GetNextTime(currentDate);
+            DateTime endDateTime = EndSessionTime.GetNextTime(currentDate);
 
             if (sessionComplete && (endDateTime <= beginDateTime))
-                return EndSessionTime.GetNextTime(currentDate, sourceTimeZoneInfo, destinationTimeZoneInfo) + TimeSpan.FromHours(24);
+                return EndSessionTime.GetNextTime(currentDate) + TimeSpan.FromHours(24);
 
-            return EndSessionTime.GetNextTime(currentDate, sourceTimeZoneInfo, destinationTimeZoneInfo);
+            return EndSessionTime.GetNextTime(currentDate);
         }
 
         /// <summary>
@@ -182,8 +182,8 @@ namespace NtCore
             bool sessionComplete = false)
         {
             DateTime[] sessionDateTimes = new DateTime[2];
-            DateTime beginDateTime = BeginSessionTime.GetNextTime(currentDate, sourceTimeZoneInfo, destinationTimeZoneInfo);
-            DateTime endDateTime = EndSessionTime.GetNextTime(currentDate, sourceTimeZoneInfo, destinationTimeZoneInfo);
+            DateTime beginDateTime = BeginSessionTime.GetNextTime(currentDate);
+            DateTime endDateTime = EndSessionTime.GetNextTime(currentDate);
 
             if (sessionComplete && (endDateTime <= beginDateTime))
                 endDateTime += TimeSpan.FromHours(24);
