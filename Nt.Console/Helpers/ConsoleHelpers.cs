eada161ce56e59059b9a49e3453539e6616b7b9b
Krumon <enriqueruedamontero@gmail.com>
2022-08-31 19:10:03 +0200

    Creating hosting services.

---
+++ Nt.Console/Helpers/ConsoleHelpers.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+    public static class ConsoleHelpers
+    {
+        public static void Title(string title)
+        {
+            string line = string.Empty;
+            foreach (char c in title)
+                line += "-";
+            Console.WriteLine(line);
+            Console.WriteLine(title.ToUpper());
+            Console.WriteLine(line);
+        }
+        
+        public static void Write(string msg = "")
+        {
+            Console.WriteLine(msg);
+        }
+
+        public static void NewLine()
+        {
+            Console.WriteLine();
+        }
+        
+        public static void Wait()
+        {
+            Console.WriteLine();
+            Console.Write("Press any key to continue...");
+            Console.ReadKey();
+            Console.WriteLine();
+        }
+
+        public static void Clear()
+        {
+            Console.Clear();
+        }
+
+        public static void WaitAndClear()
+        {
+            Console.WriteLine();
+            Console.Write("Press any key to continue...");
+            Console.ReadKey();
+            Console.Clear();
+        }
+
+    }
+}
--- Nt.Console/Program.cs
+++ Nt.Console/Program.cs
@@ -1,21 +1,72 @@
-﻿using Nt.Connect;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Options;
+using Nt.Connect;
 using Nt.Core;
 using System;
+using System.IO;
+using System.Reflection;
 using System.Threading;
+using System.Threading.Tasks;
+using static ConsoleApp.ConsoleHelpers;
 
 namespace ConsoleApp
 {
     internal class Program
     {
 
-        static SimulatorTests simulatorTests = new SimulatorTests();
-        static SessionTimeTests sessionTimeTests = new SessionTimeTests();
-        static SessionHoursTests sessionHoursTests = new SessionHoursTests();
+        //static SimulatorTests simulatorTests = new SimulatorTests();
+        //static SessionTimeTests sessionTimeTests = new SessionTimeTests();
+        //static SessionHoursTests sessionHoursTests = new SessionHoursTests();
 
-        static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            //sessionTimeTests.Run();
-            sessionHoursTests.Run();
+            using (IHost host = Host.CreateDefaultBuilder(args).ConfigureAppConfiguration((hostingContext,configurationBuilder) => 
+            {
+                configurationBuilder.Sources.Clear();
+
+                IHostEnvironment env = hostingContext.HostingEnvironment;
+                var path = "appsettings.json"; // Path.Combine(env.ContentRootPath, "appsettings.json");
+
+                configurationBuilder.SetBasePath(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location));
+
+                configurationBuilder
+                    .AddJsonFile(path, optional: true, reloadOnChange: true)
+                    .AddJsonFile($"appsettings{env.EnvironmentName}.json", optional: true, reloadOnChange: true)
+                    .AddJsonFile($"appsettingsNt.json", optional: true, reloadOnChange: true)
+                    .AddEnvironmentVariables();
+            
+                IConfigurationRoot configurationRoot = configurationBuilder.Build();
+
+                TransientFaultHandlingOptions options = new TransientFaultHandlingOptions();
+                configurationRoot.GetSection(nameof(TransientFaultHandlingOptions))
+                                 .Bind(options);
+
+                Console.WriteLine($"TransientFaultHandlingOptions.Enabled={options.Enabled}");
+                Console.WriteLine($"TransientFaultHandlingOptions.AutoRetryDelay={options.AutoRetryDelay}");
+
+            })
+            .Build())
+            {
+                //sessionTimeTests.Run();
+                //sessionHoursTests.Run();
+
+                //IConfiguration config = host.Services.GetRequiredService<IConfiguration>();
+
+                //IConfiguration config2 = new ConfigurationBuilder()
+                //    .AddJsonFile("appsettings.json",true,false)
+                //    .Build();
+
+
+                //IHostApplicationLifetime lifetime = host.Services.GetRequiredService<IHostApplicationLifetime>();
+                
+                await host.RunAsync();
+
+            }
+
+            Wait();
+
         }
 
     }
