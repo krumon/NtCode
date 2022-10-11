262c3afdd037df5b7ce01596fe956f31c79450a6
Krumon <enriqueruedamontero@gmail.com>
2022-10-11 17:59:42 +0200

    Creating NinjascriptHostedService.

---
+++ Nt.Core/Hosting/NinjascriptDI.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Nt.Core.Services;
+
+namespace Nt.Core.Hosting
+{
+    /// <summary>
+    /// The core services that could be available in the Dna Framework
+    /// for quick and easy access anywhere in code.
+    /// </summary>
+    /// <example>
+    /// <code>
+    ///     using static Dna.FrameworkDI
+    ///     
+    ///     Logger.Log(Configuration["something"]);
+    /// </code>
+    /// </example>
+    public static class NinjascriptDI
+    {
+
+        /// <summary>
+        /// Gets the configuration
+        /// </summary>
+        public static IConfiguration Configuration => NinjascriptService.Provider?.GetService<IConfiguration>();
+
+        /// <summary>
+        /// Gets the default logger
+        /// </summary>
+        public static ILogger Logger => NinjascriptService.Provider?.GetService<ILogger>();
+
+        /// <summary>
+        /// Gets the logger factory for creating loggers
+        /// </summary>
+        public static ILoggerFactory LoggerFactory => NinjascriptService.Provider?.GetService<ILoggerFactory>();
+
+        /// <summary>
+        /// Gets the framework environment
+        /// </summary>
+        public static INinjascriptEnvironment FrameworkEnvironment => NinjascriptService.Provider?.GetService<INinjascriptEnvironment>();
+
+        /// <summary>
+        /// Gets the framework exception handler
+        /// </summary>
+        public static IExceptionHandler ExceptionHandler => NinjascriptService.Provider?.GetService<IExceptionHandler>();
+    }
+}
---
+++ Nt.Core/Services/Environment/NinjascriptEnvironment.cs
@@ -0,0 +1,46 @@
+﻿using System.Diagnostics;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+namespace Nt.Core.Services
+{
+    /// <summary>
+    /// Default implementation about the current ninjascript environment
+    /// </summary>
+    public class NinjascriptEnvironment
+    {
+
+        #region Public Properties
+
+        /// <summary>
+        /// True if we are in a development (specifically, debuggable) environment
+        /// </summary>
+        public bool IsDevelopment => Assembly.GetEntryAssembly()?.GetCustomAttribute<DebuggableAttribute>()?.IsJITTrackingEnabled == true;
+
+        /// <summary>
+        /// The configuration of the environment, either Development or Production
+        /// </summary>
+        public string Configuration => IsDevelopment ? "Development" : "Production";
+
+        /// <summary>
+        /// Determines (crudely) if we are a mobile (Xamarin) platform.
+        /// This is a temporary, fragile check until it is officially supported 
+        /// https://github.com/dotnet/corefx/issues/27417
+        /// </summary>
+        public bool IsMobile => RuntimeInformation.FrameworkDescription?.ToLower().Contains("mono") == true;
+
+        #endregion
+
+        #region Constructor
+
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        public NinjascriptEnvironment()
+        {
+
+        }
+
+        #endregion
+    }
+}
