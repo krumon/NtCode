9034487e8e0e00769122a2b4fa6ca39ef420902e
Krumon <enriqueruedamontero@gmail.com>
2022-10-12 19:19:07 +0200

    Creating Hosted services.

---
+++ Nt.Core/Hosting/NinjascriptServiceCollection.cs
@@ -0,0 +1,107 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Nt.Core.Hosting
+{
+    /// <summary>
+    /// Default implementation of <see cref="INinjascriptServiceCollection"/>.
+    /// </summary>
+    public class NinjascriptServiceCollection : INinjascriptServiceCollection
+    {
+
+        public NinjascriptServiceCollection()
+        {
+            ServiceCollection sc = null;
+            ServiceDescriptor sd = null;
+            ServiceProvider sp = null;
+        }
+
+        #region Private membres
+
+        /// <summary>
+        /// Represents the descriptors collection.
+        /// </summary>
+        private readonly IList<NinjascriptServiceDescriptor> _descriptors = new List<NinjascriptServiceDescriptor>();
+
+        #endregion
+
+        #region Implamentation
+
+        /// <inheritdoc />
+        public NinjascriptServiceDescriptor this[int index]
+        {
+            get => _descriptors[index];
+            set => _descriptors[index] = value;
+        }
+
+        /// <inheritdoc />
+        public int Count => _descriptors.Count;
+
+        /// <inheritdoc />
+        public bool IsReadOnly => false;
+
+        /// <inheritdoc />
+        public void Add(NinjascriptServiceDescriptor item)
+        {
+            _descriptors.Add(item);
+        }
+
+        /// <inheritdoc />
+        public void Clear()
+        {
+            _descriptors.Clear();
+        }
+
+        /// <inheritdoc />
+        public bool Contains(NinjascriptServiceDescriptor item)
+        {
+            return _descriptors.Contains(item);
+        }
+
+        /// <inheritdoc />
+        public void CopyTo(NinjascriptServiceDescriptor[] array, int arrayIndex)
+        {
+            _descriptors.CopyTo(array, arrayIndex);
+        }
+
+        /// <inheritdoc />
+        public IEnumerator<NinjascriptServiceDescriptor> GetEnumerator()
+        {
+            return _descriptors.GetEnumerator();
+        }
+
+        /// <inheritdoc />
+        public int IndexOf(NinjascriptServiceDescriptor item)
+        {
+            return _descriptors.IndexOf(item);
+        }
+
+        /// <inheritdoc />
+        public void Insert(int index, NinjascriptServiceDescriptor item)
+        {
+            _descriptors.Insert(index, item);
+        }
+
+        /// <inheritdoc />
+        public bool Remove(NinjascriptServiceDescriptor item)
+        {
+            return _descriptors.Remove(item);
+        }
+
+        /// <inheritdoc />
+        public void RemoveAt(int index)
+        {
+            _descriptors.RemoveAt(index);
+        }
+
+        /// <inheritdoc />
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        #endregion
+
+    }
+}
