bbc0adc5af93c16a077b12b4df32a6b40aeb84a5
Krumon <enriqueruedamontero@gmail.com>
2023-01-17 19:12:48 +0100

    Creating Hosting Services.

---
+++ Nt.Core/Logging/Debugger/DebugLoggerProvider.cs
@@ -0,0 +1,19 @@
+﻿namespace Nt.Core.Logging.Debug
+{
+    /// <summary>
+    /// The provider for the <see cref="DebugLogger"/>.
+    /// </summary>
+    [ProviderAlias("Debug")]
+    public class DebugLoggerProvider : ILoggerProvider
+    {
+        /// <inheritdoc />
+        public ILogger CreateLogger(string name)
+        {
+            return new DebugLogger(name);
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
