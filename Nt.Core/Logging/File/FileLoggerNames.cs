14c626ebdbb6db5b1bdaa8b75b9942dc0b39c59d
Krumon <enriqueruedamontero@gmail.com>
2023-01-17 20:11:26 +0100

    Creating Hosting services.

---
+++ Nt.Core/Logging/File/FileLoggerNames.cs
@@ -0,0 +1,23 @@
+﻿namespace Nt.Core.Logging.File
+{
+    ///// <summary>
+    ///// Reserved formatter names for the built-in console formatters.
+    ///// </summary>
+    public static class FileLoggerNames
+    {
+        ///// <summary>
+        ///// Reserved name for simple console formatter
+        ///// </summary>
+        //public const string Boot = "boot.log";
+
+        ///// <summary>
+        ///// Reserved name for json console formatter
+        ///// </summary>
+        //public const string Messages = "messages.log";
+
+        ///// <summary>
+        ///// Reserved name for systemd console formatter
+        ///// </summary>
+        //public const string Secure = "secure.log";
+    }
+}
