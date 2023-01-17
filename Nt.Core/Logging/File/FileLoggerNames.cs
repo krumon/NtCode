cac923ac25d2e6848697d2fb13f3a5398aff1398
Krumon <enriqueruedamontero@gmail.com>
2023-01-17 20:14:28 +0100

    Creating hosting services.

--- Nt.Core/Logging/File/FileLoggerNames.cs
+++ Nt.Core/Logging/File/FileLoggerNames.cs
@@ -1,23 +1,23 @@
 ﻿namespace Nt.Core.Logging.File
 {
-    ///// <summary>
-    ///// Reserved formatter names for the built-in console formatters.
-    ///// </summary>
+    /// <summary>
+    /// Reserved file names for the built-in file loggers.
+    /// </summary>
     public static class FileLoggerNames
     {
-        ///// <summary>
-        ///// Reserved name for simple console formatter
-        ///// </summary>
-        //public const string Boot = "boot.log";
+        /// <summary>
+        /// El archivo boot.log almacena la información relacionada con el estado del arranque del sistema.
+        /// </summary>
+        public const string Boot = "boot.log";
 
-        ///// <summary>
-        ///// Reserved name for json console formatter
-        ///// </summary>
-        //public const string Messages = "messages.log";
+        /// <summary>
+        /// Este archivo también es conocido como syslog y contiene la información diaria de como va funcionando nuestro sistema. Es un log de propósito general de varios servicios que utilizan este archivo. Este archivo es un buen lugar en donde iniciar a buscar errores cuando un problema se presenta.
+        /// </summary>
+        public const string Messages = "messages.log";
 
-        ///// <summary>
-        ///// Reserved name for systemd console formatter
-        ///// </summary>
-        //public const string Secure = "secure.log";
+        /// <summary>
+        /// Este archivo contiene logs relacionados a la seguridad. Cada vez que un usuario ejecuta comandos con un nivel superior, se almacena el comando ejecutado dentro del secure log. Si el usuario no puede completar un login, esta acción también es almacenada dentro de este archivo.
+        /// </summary>
+        public const string Secure = "secure.log";
     }
 }
