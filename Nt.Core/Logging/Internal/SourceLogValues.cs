1d4ed26715918aa993593f14b21e54de8397b8c7
Krumon <enriqueruedamontero@gmail.com>
2023-01-19 16:25:44 +0100

    Creating hosting services.

--- Nt.Core/Logging/Internal/SourceLogValues.cs
+++ Nt.Core/Logging/Internal/SourceLogValues.cs
@@ -10,21 +10,38 @@ namespace Nt.Core.Logging.Internal
     /// LogValues to enable formatting options supported by <see cref="string.Format(IFormatProvider, string, object?)"/>.
     /// This also enables using {NamedformatItem} in the format string.
     /// </summary>
-    internal readonly struct SourceLogValues 
+    internal readonly struct SourceLogValues
     {
         internal const int MaxCachedFormatters = 1024;
         private const string NullFormat = "[null]";
         private static int _count;
-        //private static ConcurrentDictionary<string, LogValuesFormatter> _formatters = new ConcurrentDictionary<string, LogValuesFormatter>();
-        private readonly string _formatter;
+        private static ConcurrentDictionary<string, LogValuesSourceFormatter> _formatters = new ConcurrentDictionary<string, LogValuesSourceFormatter>();
+        private readonly LogValuesSourceFormatter _formatter;
         private readonly object[] _values;
         private readonly string _originalMessage;
+
+        // for testing purposes
+        internal LogValuesSourceFormatter Formatter => _formatter;
 
         public SourceLogValues(string format, params object[] values)
         {
             if (values != null && values.Length != 0 && format != null)
             {
-                _formatter = string.Empty;
+                if (_count >= MaxCachedFormatters)
+                {
+                    if (!_formatters.TryGetValue(format, out _formatter))
+                    {
+                        _formatter = new LogValuesSourceFormatter(format);
+                    }
+                }
+                else
+                {
+                    _formatter = _formatters.GetOrAdd(format, f =>
+                    {
+                        Interlocked.Increment(ref _count);
+                        return new LogValuesSourceFormatter(f);
+                    });
+                }
             }
             else
             {
