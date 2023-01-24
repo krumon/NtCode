2bb2c3725b4f027fa1ffc17fdf42835bf7d5829d
Krumon <enriqueruedamontero@gmail.com>
2023-01-24 13:53:27 +0100

    Creating logging services.

--- Nt.Core/Options/OptionsFactory.cs
+++ Nt.Core/Options/OptionsFactory.cs
@@ -75,7 +75,7 @@ namespace Nt.Core.Options
                 }
                 if (failures.Count > 0)
                 {
-                    throw new Exception($"OptionsValidationException: {nameof(name)}, {typeof(TOptions)}, {nameof(failures)}");
+                    throw new Exception($"Options Validation Exception: {nameof(name)}, {typeof(TOptions)}, {nameof(failures)}");
                 }
             }
 
