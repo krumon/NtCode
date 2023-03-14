ba87a6fa51935d9e33224d0919e506228659a73b
Krumon <enriqueruedamontero@gmail.com>
2023-03-14 19:56:31 +0100

    Creating ninjascript service.

---
+++ Nt.Scripts/Ninjascripts/NinjascriptOptions.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace Nt.Scripts.Ninjascripts
+{
+    public class NinjascriptOptions
+    {
+        public Dictionary<string, Dictionary<string,bool>> Add { get; set; }
+    }
+}
